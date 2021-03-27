0258054d0630a71c3b82af03ff79699da9ed2eb0
knn <nguyenkhoi2190@gmail.com>
2021-03-27 12:06:02 +0800

    Add `Good vs Evil` challenge

---
+++ CodeWarsKatas/CodeWarsKatas/CodeWarsKatas.h
@@ -0,0 +1,18 @@
+//
+//  CodeWarsKatas.h
+//  CodeWarsKatas
+//
+//  Created by Khoi Nguyen on 27/3/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CodeWarsKatas.
+FOUNDATION_EXPORT double CodeWarsKatasVersionNumber;
+
+//! Project version string for CodeWarsKatas.
+FOUNDATION_EXPORT const unsigned char CodeWarsKatasVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodeWarsKatas/PublicHeader.h>
+
+
